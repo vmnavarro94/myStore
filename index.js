617150348d1a74ee884829049293b2afdb3cb942
vmnavarro94 <vm.navarro94@gmail.com>
2022-01-23 22:01:29 -0600

    example routes

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('hola mi server en express')
 })
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('hola nueva ruta')
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: '1000'
+  })
+})
+
 app.listen(port, () => {
   console.log('Mi port' + port)
 })

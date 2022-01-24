49ef2e37b2f21f5734ad9a56d4dc63a7802e6b56
vmnavarro94 <vm.navarro94@gmail.com>
2022-01-23 22:18:27 -0600

    mock params routes

--- index.js
+++ index.js
@@ -6,14 +6,33 @@ app.get('/', (req, res) => {
   res.send('hola mi server en express')
 })
 
-app.get('/nueva-ruta', (req, res) => {
-  res.send('hola nueva ruta')
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Producto 1',
+      price: 1000,
+    },
+    {
+      name: 'Producto 2',
+      price: 10,
+    }
+  ])
 })
 
-app.get('/products', (req, res) => {
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
   res.json({
+    id,
     name: 'Producto 1',
-    price: '1000'
+    price: 1000
+  })
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params
+  res.json({
+    categoryId,
+    productId,
   })
 })
 

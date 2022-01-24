41cbeac25204c9f92c526c68b6d40c902fba927f
vmnavarro94 <vm.navarro94@gmail.com>
2022-01-24 12:22:37 -0600

    configure cors

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express')
+const cors = require('cors')
 
 const routerApi = require('./routes')
-const { logErrors, errorHandler, boomErrorHangler } = require('./middlewares/error.handler')
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHangler
+} = require('./middlewares/error.handler')
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+
+/*
+const whitelist = ['http://localhost:8080', 'https://myapp.com']
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('no permitido'))
+    }
+  }
+}
+*/
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('hola mi server en express')

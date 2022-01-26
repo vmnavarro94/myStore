d6135e47d61ca13c1e4c6c99fd4526be2fa5bc45
vmnavarro94 <vm.navarro94@gmail.com>
2022-01-26 17:34:48 -0600

    First migration

--- db/config.js
+++ db/config.js
@@ -2,7 +2,7 @@ const { config } = require('../config/config')
 
 const USER = encodeURIComponent(config.dbUser)
 const PASSWORD = encodeURIComponent(config.dbPassword)
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/`
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
 module.exports = {
   development: {
